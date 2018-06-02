f0fbfad9519640541357902ca9e3f15101aa5944
Pietari Heino <ph@extreg.com>
2018-06-02 21:16:20 +0300

    connection component now supports different shapes

--- client/src/components/Connection.vue
+++ client/src/components/Connection.vue
@@ -2,13 +2,11 @@
 <template>
 
 
-  <path :d="`M ${x1 + rand1} ${y1 - rand2}
-             l ${ sign * Math.abs(x1 - x2 + rand1) / 2 } 0
-             l 0 ${y2 - y1 + rand2}
-             L ${x2 - rand1} ${y2 + rand2}`"
+  <path :d="pathString"
+
         fill="transparent"
-        stroke="black"
-        stroke-width="1"
+        :stroke="userAdded ? '#9e9e9e' : 'green'"
+        stroke-width="3px"
         :stroke-dasharray="dashParams">
   </path>
 
@@ -32,31 +30,45 @@ export default {
     return {
       deltaX: 125,
       deltaY: 60,
-      rand1: Math.floor(Math.random() * 4),
-      rand2: Math.floor(Math.random() * 5),
     };
   },
   created() {},
   computed: {
+    pathString() {
+      const shape = this.$store.state.UI.pathShape;
+      const x1 = this.x1;
+      const y1 = this.y1;
+      const x2 = this.x2;
+      const y2 = this.y2;
+      const rand1 = Math.floor(Math.random() * 4);
+      const rand2 = Math.floor(Math.random() * 5);
+      const sign = x1 > x2 ? -1 : 1
 
+      if (shape === 'curve') {
+        return `M ${x1} ${y1}
+                Q ${x1 + (x2-x1)/2} ${y1 + (y1 > y2 ? -100 : 100) + (y2-y1)/2}
+                ${x2} ${y2}`
+      } else if (shape === 'path') {
+        return `M ${x1 + rand1} ${y1 - rand2}
+                 l ${ sign * Math.abs(x1 - x2 + rand1) / 2 } 0
+                 l 0 ${y2 - y1 + rand2}
+                 L ${x2 - rand1} ${y2 + rand2}`;
+      } else {
+        return `M ${x1} ${y1} L ${x2} ${y2}`;
+      }
+    },
     posFrom() {
       return this.$store.getters.posById(this.from);
     },
     posTo() {
       return this.$store.getters.posById(this.to);
     },
-
     sizeFrom() {
       return this.$store.getters.containerSize(this.from);
     },
     sizeTo() {
       return this.$store.getters.containerSize(this.to);
     },
-
-    sign() {
-      return this.x1 > this.x2 ? -1 : 1
-    },
-
     x1() {
         return this.middle(this.from).x;
     },
@@ -70,10 +82,10 @@ export default {
       return this.middle(this.to).y;
     },
     dashParams() {
-      if (this.to[0] === this.from[0]) {
-        return "0,0";
-      }
-      return "2,2";
+      return "10px 3px";
+    },
+    userAdded() {
+      return this.to[0] === this.from[0];
     },
   },
   methods: {
